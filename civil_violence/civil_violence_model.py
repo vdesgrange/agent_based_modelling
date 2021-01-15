47f90d9f94cb623ab692590a8f9ecbf453d5a20c
Viviane Desgrange <viviane.desgrange@gmail.com>
2021-01-15 18:46:13 +0100

    Initial work on civil violence model. Basic functions of the model with Epstein 2002 attributes. Update server to handle new model.

--- civil_violence/civil_violence_model.py
+++ civil_violence/civil_violence_model.py
@@ -1,11 +1,84 @@
 from mesa import Model
+from mesa.space import MultiGrid
+from mesa.time import RandomActivation
+from mesa.datacollection import DataCollector
 
 
 class CivilViolenceModel(Model):
-    """
-    Civil violence model
-    """
-    def __init__(self, height=40, width=40):
+    """ Civil violence model class """
+    def __init__(self,
+                 height, width,
+                 agent_density, agent_vision,
+                 cop_density, cop_vision,
+                 initial_legitimacy_l0, max_iter,
+                 max_jail_term, active_threshold_t, k):
+        """
+        Create a new civil violence model.
+
+        :param height: Grid height.
+        :param width: Grid width.
+        :param agent_density: Approximate percentage of cells occupied by citizen agents.
+        :param agent_vision: Radius of the agent vision in every direction.
+        :param cop_density: Approximate percentage of cells occupied by cops.
+        :param cop_vision: Radius of the cop vision in every direction.
+        :param initial_legitimacy_l0: Initial legitimacy of the central authority.
+        :param max_iter: Maximum number of steps in the simulation.
+        :param max_jail_term: Maximal jail term.
+        :param active_threshold_t: Threshold where citizen agent became active.
+        :param k: Arrest term constant k.
+        """
+
         super().__init__()
+
+        # Initialize Model grid and schedule
         self.height = height
         self.width = width
+        self.grid = MultiGrid(self.width, self.height, torus=True)  # Grid or MultiGrid ?
+        self.schedule = RandomActivation(self)
+        self.max_iter = max_iter
+        self.iteration = 0  # Simulation iteration counter
+        self.running = True
+
+        # Set Model main attributes
+        self.max_jail_term = max_jail_term
+        self.active_threshold_t = active_threshold_t
+        self.initial_legitimacy_l0 = initial_legitimacy_l0
+        self.k = k
+
+        self.agent_density = agent_density
+        self.agent_vision = agent_vision
+        self.cop_density = cop_density
+        self.cop_vision = cop_vision
+
+        # Data collection
+        self.data_collector = DataCollector(
+            model_reporters=self.get_model_reporters(),
+            agent_reporters=self.get_agent_reporters()
+        )
+
+    def step(self):
+        """ One step in agent-based model simulation """
+        self.schedule.step()
+        self.data_collector.collect(self)
+        self.iteration += 1
+
+        if self.iteration > self.max_iter:
+            self.running = False
+
+    def get_model_reporters(self):
+        """ Dictionary of model reporter names and attributes/funcs """
+        return {}
+
+    def get_agent_reporters(self):
+        """ Dictionary of agent reporter names and attributes/funcs """
+        return {}
+
+    def count_type_citizens(model, condition, exclude_jailed=True):
+        """
+        Helper method to count agents.
+        Cop agents can't disappear from the map, so number of cops can be retrieved from model attributes.
+        TODO
+        """
+        count = 0
+
+        return count
--- civil_violence/server.py
+++ civil_violence/server.py
@@ -3,19 +3,44 @@ from mesa.visualization.UserParam import UserSettableParameter
 from civil_violence_model import CivilViolenceModel
 
 
-def get_model_parameters():
+def get_manual_model_parameters():
     """
     Get parameters of the agent-based model
+    Default parameters based on Run 5 of Epstein type 1 civil violence model.
     :return: A dictionary of model parameters
     """
-    return {}
+    return {
+        "agent_density": UserSettableParameter("slider", "Agent Density", 70, 0, 100,
+                                                 description="Initial percentage of citizen in population"),
+        "cop_density": UserSettableParameter("slider", "Cop Density", 7.4, 0, 100,
+                                             description="Initial percentage of cops in population"),
+        "agent_vision": UserSettableParameter("slider", "Agent Vision", 7, 0, 10,
+                                              description="Number of patches visible to citizens"),
+        "cop_vision": UserSettableParameter("slider", "Cop Vision", 7, 0, 10,
+                                            description="Number of patches visible to cops"),
+        "initial_legitimacy_l0": UserSettableParameter("slider", "Initial Central authority legitimacy", 80, 0, 100,
+                                                       description="Global parameter: Central authority legitimacy"),
+        "active_threshold_t": UserSettableParameter("slider", "Active Threshold", 10, 0, 100,
+                                                  description="Threshold that agent's Grievance must exceed Net Risk to go active"),
+        "max_jail_term": UserSettableParameter("slider", "Max Jail Term", 1000, 0, 1000,
+                                               description="Maximum number of steps that jailed citizens stay in")
+    }
 
 
 def run():
     """
     Run the mesa server
     """
-    model_params = get_model_parameters()
+
+    model_params = {
+        "width": 40,
+        "height": 40,
+        "max_iter": 1000,
+        "max_jail_term": 1000,
+        "k": 2.3,
+    }
+    model_params.update(get_manual_model_parameters())
+
     server = ModularServer(CivilViolenceModel, [], name="Civil violence with network model", model_params=model_params)
 
     server.port = 8521
