d6abebbd271a52e581b34a2fa4ba5ebbc0c7207d
Floris de Vries <54944883+Florisdv5@users.noreply.github.com>
2021-01-27 13:25:26 +0100

    Update SA.py

--- civil_violence/SA.py
+++ civil_violence/SA.py
@@ -1,7 +1,6 @@
 from IPython.display import clear_output
 import SALib
 
-# %matplotlib inline
 from SALib.sample import saltelli
 from mesa.batchrunner import BatchRunner
 from SALib.analyze import sobol
@@ -17,16 +16,18 @@ from utils import read_configuration
 
 path = 'archives/saved_data{0}.npy'.format(int(time.time()))
 
+number_of_variables = 6
+
 problem = {
-    'num_vars': 3,
+    'num_vars': number_of_variables,
     'names': ['active_threshold_t', 'initial_legitimacy_l0',
-              'max_jail_term'],
-    'bounds': [[0.01, 1], [0.01, 1], [1, 100]]
+              'max_jail_term', 'p', 'agent_vision', 'cop_vision'],
+    'bounds': [[0.01, 1], [0.01, 1], [1, 100], [0.01, 0.4], [1, 20], [1, 20]]
 }
 
-replicates = 100
+replicates = 10
 max_steps = 200
-distinct_samples = 5
+distinct_samples = 20
 
 model_reporters = {"QUIESCENT": lambda m: m.count_type_citizens("QUIESCENT"),
                    "ACTIVE": lambda m: m.count_type_citizens("ACTIVE"),
@@ -42,6 +43,10 @@ for i, var in enumerate(problem['names']):
     # Keep in mind that max_jail_term should be integers. You will have to change
     # your code to acommodate for this or sample in such a way that you only get integers.
     if var == 'max_jail_term':
+        samples = np.linspace(*problem['bounds'][i], num=distinct_samples, dtype=int)
+    if var == 'agent_vision':
+        samples = np.linspace(*problem['bounds'][i], num=distinct_samples, dtype=int)
+    if var == 'cop_vision':
         samples = np.linspace(*problem['bounds'][i], num=distinct_samples, dtype=int)
 
     configuration = read_configuration()
@@ -100,7 +105,7 @@ def plot_all_vars(df, param):
         param: the parameter to be plotted
     """
 
-    f, axs = plt.subplots(3, figsize=(7, 10))
+    f, axs = plt.subplots(number_of_variables, figsize=(7, 10))
 
     for i, var in enumerate(problem['names']):
         plot_param_var_conf(axs[i], df[var], var, param, i)
