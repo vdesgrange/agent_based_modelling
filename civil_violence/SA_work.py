7615e0ac00c172c83b43a9b8a7b1013b904d9810
Daniël Vink <danielvink@pop-os.localdomain>
2021-01-27 22:16:27 +0100

    DV: Working calculations

--- civil_violence/SA_work.py
+++ civil_violence/SA_work.py
@@ -7,10 +7,22 @@ THRESHOLD   = 150
 ITERATIONS  =  10
 STEPS       =  20
 N_PARAMS    =   6
+N_OUTPUT    =   6
 
 PARAMS = ['active_threshold_t', 'initial_legitimacy_l0',
               'max_jail_term', 'p', 'agent_vision', 'cop_vision']
 BOUNDS = [[0.01, 1], [0.01, 1], [1, 100], [0.01, 0.4], [1, 20], [1, 20]]
+OUTPUT_PARAMS = ['PARAM_VAL', 'MEAN_N', 'MEAN_PEAK_HEIGHT', 'MEAN_PEAK_WIDTH', 
+                'MAX_PEAK_HEIGHT', 'MAX_PEAK_WIDTH']
+
+### TEST SETTINGS ###
+ITERATIONS = 2
+STEPS = 2
+PARAMS = ['active_threshold_t', 'initial_legitimacy_l0','max_jail_term']
+BOUNDS = [[0, 1], [0, 1], [0, 100]]
+
+#####################
+
 
 
 file_paths = [
@@ -30,7 +42,7 @@ def load_datacollector():
 
             data = np.load(f, allow_pickle = True)[()]
             keys = data.keys()
-            print('Keys of this dict: ', keys, '\n')
+            # print('Keys of this dict: ', keys, '\n')
             data_dict[path] = data
     
     return data_dict
@@ -40,14 +52,23 @@ def map_keys():
     Maps the used parameter bounds to key values for data dictionary.
     """
     keys_dict = {}
-    iter_list = list(range(ITERATIONS))
+    iter_list = np.arange(ITERATIONS*STEPS).reshape((STEPS, ITERATIONS))
     for i in range(len(PARAMS)):
-        param_range = np.linspace(BOUNDS[i][0], BOUNDS[i][1], STEPS)
         param_list = []
-        for pr in param_range:
-            for j in iter_list:
-                param_list.append((pr, j))
+        param_range = np.linspace(BOUNDS[i][0], BOUNDS[i][1], STEPS).reshape(STEPS, 1)
+        param_matrix = np.repeat(param_range, ITERATIONS, axis=1)
+        for j in range(STEPS):
+            param_list.extend(list(zip(param_matrix[j], iter_list[j])))
         keys_dict[PARAMS[i]] = param_list
+    # print(iter_list)
+    # for i in range(len(PARAMS)):
+    #     param_range = np.linspace(BOUNDS[i][0], BOUNDS[i][1], STEPS)
+    #     param_list = []
+    #     for pr in param_range:
+    #         for j in iter_list[i*ITERATIONS:(i+1)*ITERATIONS]:
+    #             print('j: ', pr, j)
+    #             param_list.append((pr, j))
+    #     keys_dict[PARAMS[i]] = param_list
     return keys_dict
 
 
@@ -68,13 +89,40 @@ def get_param_means(data, parameter):
     """
     
     """
+    output = np.zeros((STEPS, N_OUTPUT))
+    peak_heights = []
+    peak_widths = []
     keys = keys_dict[parameter]
-    print(keys)
-    # print(data[parameter][(0.0, 0)]['ACTIVE'])
+    # print('KEYS: ', keys)
 
-def show_active(data):
-    actives = np.array(data['inf_threshold'])
-    # print(np.shape(actives), '\n', actives)
+    for i in range(STEPS):
+        s_keys = keys[i*ITERATIONS : (i+1)*ITERATIONS]
+        # print('s_keys: ', s_keys)
+    
+        for key in s_keys:
+            actives = data[parameter][key]['ACTIVE']
+            ph, pw = get_outbreaks(actives, THRESHOLD)
+            peak_heights.extend(ph)
+            peak_widths.extend(pw)
+            # print(key, ' success')
+        mean_n_peaks = len(peak_heights)/ITERATIONS
+        mean_peak_height = np.mean(np.array(peak_heights))
+        mean_peak_width = np.mean(peak_widths)
+        max_peak_height = np.max(peak_heights)
+        max_peak_width = np.max(peak_widths)
+
+        output[i] = [key[0], mean_n_peaks, mean_peak_height, mean_peak_width, max_peak_height, max_peak_width]
+        # print(parameter)
+        # print('MEAN_N | MEAN_PEAK_HEIGHT | MEAN_PEAK_WIDTH | MAX_PEAK_HEIGHT | MAX_PEAK_WIDTH ')
+        # print(mean_n_peaks, mean_peak_height, mean_peak_width, max_peak_height, max_peak_width)
+    # print(data[parameter][(0.0, 0)]['ACTIVE'])
+    # print(output)
+
+    df = pd.DataFrame({'PARAM_VAL': output[:, 0], 'MEAN_N': output[:, 1], 
+        'MEAN_PEAK_HEIGHT': output[:, 2], 'MEAN_PEAK_WIDTH': output[:, 3], 
+        'MAX_PEAK_HEIGHT': output[:, 4], 'MAX_PEAK_WIDTH': output[:, 5]})
+    return df
+    # return output, df
 
 def get_outbreaks(data, threshold):
     """
@@ -113,15 +161,21 @@ def get_outbreaks(data, threshold):
 model_data = load_datacollector()
 run_data = model_data['./archives/saved_data1611747993_run']
 keys_dict = map_keys()
+output_data = {}
 
 for param in run_data.keys():
-    get_param_means(run_data, param)
+    output_data[param] = get_param_means(run_data, param)
 
-# print(model_data['./archives/saved_data1611693859']['active_threshold_t'])
-for d in model_data:
-    print(model_data[d]['active_threshold_t'].keys())
 
-test_data = np.array([10, 20, 30, 80, 201, 75, 100, 120, 220, 201, 190, 100, 80])
-peaks, sizes = get_outbreaks(test_data, 100)
+for df in output_data:
+    print('Tested parameter: ', df)
+    print(output_data[df])
 
-print(peaks, sizes)
+# # print(model_data['./archives/saved_data1611693859']['active_threshold_t'])
+# for d in model_data:
+#     print(model_data[d]['active_threshold_t'].keys())
+
+# test_data = np.array([10, 20, 30, 80, 201, 75, 100, 120, 220, 201, 190, 100, 80])
+# peaks, sizes = get_outbreaks(test_data, 100)
+
+# print(peaks, sizes)
