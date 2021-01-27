bbe7f37ed2c0ffa5980c38a68e61b2f5dc560ac4
Floris de Vries <54944883+Florisdv5@users.noreply.github.com>
2021-01-27 13:25:42 +0100

    Update civil_violence_model.py

--- civil_violence/civil_violence_model.py
+++ civil_violence/civil_violence_model.py
@@ -13,13 +13,13 @@ from figure import create_fig
 class CivilViolenceModel(Model):
     """ Civil violence model class """
     def __init__(self,
-                 height, width,
-                 agent_density, agent_vision,
-                 active_agent_density,
-                 cop_density, cop_vision, inf_threshold,
-                 removal_step, max_iter,
-                 k, graph_type,
-                 p, p_ws, directed,
+                 height=40, width=40,
+                 agent_density=0.7, agent_vision=7,
+                 active_agent_density=0.01,
+                 cop_density=0.04, cop_vision=7, inf_threshold=10,
+                 removal_step=0, max_iter=200,
+                 k=2.3, graph_type="None",
+                 p=0.1, p_ws=0.1, directed=False,
                  max_jail_term=30, active_threshold_t=0.1,
                  initial_legitimacy_l0=0.82,
                  movement=True, seed=None):
