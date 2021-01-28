a71329c34c12b56207863eabfbbe6927b214539d
Viviane Desgrange <viviane.desgrange@gmail.com>
2021-01-28 02:54:58 +0100

    Work in progress.

--- civil_violence/Sobol.py
+++ civil_violence/Sobol.py
@@ -12,9 +12,9 @@ from utils import *
 
 
 def sobol_analysis_no_network(problem):
-    replicates = 20
-    max_steps = 200
-    distinct_samples = 20
+    replicates = 2
+    max_steps = 2
+    distinct_samples = 2
 
     path = 'archives/saved_data_sobol_{0}.npy'.format(int(time.time()))
 
@@ -30,24 +30,72 @@ def sobol_analysis_no_network(problem):
                         columns=['active_threshold_t', 'initial_legitimacy_l0',
                                  'max_jail_term', 'agent_vision', 'cop_vision'])
 
-    data['Run'], data['QUIESCENT'], data['ACTIVE'], data['JAILED'], data['OUTBREAKS'] =\
-        None, None, None, None, None
+    data['Run'], data['QUIESCENT'], data['ACTIVE'], data['JAILED'], data['OUTBREAKS'], data['LEGITIMACY'] =\
+        None, None, None, None, None, None
 
+    column_order = ['Run', 'QUIESCENT', 'ACTIVE', 'JAILED', 'OUTBREAKS', 'LEGITIMACY']
     available_processors = cpu_count()
     pool = Pool(available_processors)
 
-    combinations = enumerate([list(v) for _ in range(replicates) for v in param_values])
+    run_iter_args = enumerate([[max_steps, model_reporters, list(v)] for _ in range(replicates) for v in param_values])
 
     with tqdm(replicates, disable=False) as pbar:
-        for count, vals, iteration_data in pool.imap_unordered(_mp_function, combinations):
+        for count, vals, iteration_data in pool.imap_unordered(_mp_function, run_iter_args):
             data.iloc[count, 0:5] = vals
-            data.iloc[count, 5:10] = iteration_data
+            data.loc[count, column_order] = iteration_data[column_order]
             pbar.update()
 
+    # Close multi-processing
     pool.close()
 
     with open(path, 'ab') as f:
         np.save(f, data)
+        print("Results saved in file {:s}".format(path))
+
+    return data
+
+
+def _mp_function(input):
+    count, iter_args = input
+
+    max_steps = iter_args[0]
+    model_reporters = iter_args[1]
+    vals = iter_args[2]
+
+    vals[2] = int(vals[2])
+    vals[3] = int(vals[3])
+    vals[4] = int(vals[4])
+
+    variable_parameters = {}
+    names = ['active_threshold_t', 'initial_legitimacy_l0', 'max_jail_term', 'agent_vision', 'cop_vision']
+    for name, val in zip(names, vals):
+        variable_parameters[name] = val  # dictionary
+
+    batch = BatchRunner(CivilViolenceModel,
+                        max_steps=max_steps,
+                        variable_parameters={name: [] for name in names},
+                        model_reporters=model_reporters)
+
+    batch.run_iteration(variable_parameters, tuple(vals), count)
+    iteration_data = batch.get_model_vars_dataframe()
+    iteration_data['Run'] = count  # Don't know what causes this, but iteration number is not correctly filled
+
+    return count, vals, iteration_data
+
+
+def sobol_main():
+    problem = {
+        'num_vars': 5,
+        'names': ['active_threshold_t', 'initial_legitimacy_l0', 'max_jail_term', 'agent_vision', 'cop_vision'],
+        'bounds': [[0.01, 1], [0.01, 1], [1, 100], [1, 20], [1, 20]]
+    }
+
+    sobol_analysis_no_network(problem)
+
+
+if __name__ == '__main__':
+    sobol_main()
+
 
 
     # batch = BatchRunner(CivilViolenceModel,
@@ -76,46 +124,3 @@ def sobol_analysis_no_network(problem):
     #         count += 1
     #
     #         print(f'{count / (len(param_values) * (replicates)) * 100:.2f}% done')
-
-    # Close multi-processing
-
-    return data
-
-
-def _mp_function(input):
-    count, vals = input
-
-    vals[2] = int(vals[2])
-    vals[3] = int(vals[3])
-    vals[4] = int(vals[4])
-
-    variable_parameters = {}
-    names = ['active_threshold_t', 'initial_legitimacy_l0', 'max_jail_term', 'agent_vision', 'cop_vision']
-    for name, val in zip(names, vals):
-        variable_parameters[name] = val  # dictionary
-
-    batch = BatchRunner(CivilViolenceModel,
-                        max_steps=max_steps,
-                        variable_parameters={name: [] for name in names},
-                        model_reporters=model_reporters)
-
-    batch.run_iteration(variable_parameters, tuple(vals), count)
-    iteration_data = batch.get_model_vars_dataframe().iloc[count]
-    iteration_data['Run'] = count  # Don't know what causes this, but iteration number is not correctly filled
-
-    print("Counter : ", count)
-    return count, vals, iteration_data
-
-
-def sobol_main():
-    problem = {
-        'num_vars': 5,
-        'names': ['active_threshold_t', 'initial_legitimacy_l0', 'max_jail_term', 'agent_vision', 'cop_vision'],
-        'bounds': [[0.01, 1], [0.01, 1], [1, 100], [1, 20], [1, 20]]
-    }
-
-    data = sobol_analysis_no_network(problem)
-
-
-if __name__ == '__main__':
-    sobol_main()
